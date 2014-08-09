f0afc654c8aba6b0f21d89dcd0d7fbf9a0958b9e
turk <joseph.r.turk@gmail.com>
2014-08-09 14:53:34 -0400

    Added OAuthSettings.py to project.

---
+++ OAuthSettings.py
@@ -0,0 +1,6 @@
+settings = {
+    'consumer_key': 'MfkERfdX3nRntyy4zlP4aEQmh',
+    'consumer_secret': '8ugD9pyKf8lvtfcHmNMVHaqXfTtkDFtCY7Ya0vIEhoYdw3K06l',
+    'access_token_key': '2715891913-QFz5GJ0OEYhbObyTucVhIKlQXeZH3RgIU18dSE0',
+    'access_token_secret': 'DD1BmGXEZAGzak5utVQffhPgBNyVXzIMBlmwCJuPHnv3P'
+}
