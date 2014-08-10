99682430f37519180631a03f23cfcfe082189dfc
turk <joseph.r.turk@gmail.com>
2014-08-10 17:57:51 -0400

    Removing OAuthSettings from repo and adding an ignore file

--- OAuthSettings.py
+++
@@ -1,6 +0,0 @@
-settings = {
-    'consumer_key': '6RHrOwmRVSVMa4dSj08LSGA41',
-    'consumer_secret': 'T1JZHKFuBnEWGVNXZSHizFM29FdH02UMldKwhWEbW3wytn8wlJ',
-    'access_token_key': '2715891913-PvNWKoTwkaPPitsYUvRNxjbdjhG1g0OCFMIDFEp',
-    'access_token_secret': 'Zmvzp33KUHoLCSsqqAeHMigd46lS5qMXt2eA8d33ghant'
-}
